bd56ad1c0c9d5a4f50ef8bae21a3505588bf283d
sanyancha <31632907+sanyancha@users.noreply.github.com>
2024-06-02 10:54:07 +0300

    Start React project

---
+++ src/header.jsx
@@ -0,0 +1,11 @@
+import logo from './assets/img/logo.svg'
+import cart from './assets/img/cart.svg'
+
+export const Header = () => {
+    return (
+        <header className='header'>  
+         <img src={logo} alt="logo"/>
+         <img src={cart} alt="cart"/>
+        </header>
+    )
+}
